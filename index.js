0539b1d7024d6050e96b11e34f6256b838153969
ShawonECE <send2shawon@gmail.com>
2024-08-16 18:21:53 +0600

    filtering implemented

--- index.js
+++ index.js
@@ -35,28 +35,54 @@ async function run() {
         });
 
         app.get('/products', async (req, res) => {
+            // console.log(req.query);
             const name = req.query?.name;
             const skip = req.query?.skip;
             const limit = req.query?.limit;
             const sort = req.query?.sort;
+            const brand = req.query?.brand;
+            const min_price = req.query?.min_price;
+            const max_price = req.query?.max_price;
+            const category = req.query?.category;
+            const query = {};
+            if (brand && brand !== 'all') {
+                query.brand = brand;
+            }
+            if (category && category !== 'all') {
+                query.category = category;
+            }
+            if (min_price && min_price !== 'no') {
+                if (query.price) {
+                    query.price.$gte = Number(min_price);
+                } else {
+                    query.price = { $gte: Number(min_price) };
+                }
+            }
+            if (max_price && max_price !== 'no') {
+                if (query.price) {
+                    query.price.$lte = Number(max_price);
+                } else {
+                    query.price = { $lte: Number(max_price) };
+                }
+            }
             if (name) {
                 const result = await productsColl.find().toArray();
                 return res.send(result.filter(product => product.product_name.toLowerCase().includes(name.toLowerCase())));
             }
             if (sort === 'low_price_first') {
-                const result = await productsColl.find().sort({ price: 1 }).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
+                const result = await productsColl.find(query).sort({ price: 1 }).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
                 res.send(result);
             } else if (sort === 'high_price_first') {
-                const result = await productsColl.find().sort({ price: -1 }).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
+                const result = await productsColl.find(query).sort({ price: -1 }).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
                 res.send(result);
             } else if (sort === 'old_date_first') {
-                const result = await productsColl.find().sort({ adding_date: 1 }).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
+                const result = await productsColl.find(query).sort({ adding_date: 1 }).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
                 res.send(result);
             } else if (sort === 'new_date_first') {
-                const result = await productsColl.find().sort({ adding_date: -1 }).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
+                const result = await productsColl.find(query).sort({ adding_date: -1 }).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
                 res.send(result);
             } else {
-                const result = await productsColl.find().skip(parseInt(skip)).limit(parseInt(limit)).toArray();
+                const result = await productsColl.find(query).skip(parseInt(skip)).limit(parseInt(limit)).toArray();
                 res.send(result);
             }
         });
